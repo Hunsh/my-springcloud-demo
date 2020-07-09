ae0f4fa3b1492cc3bec47e797ac9d289d766c1fb
jiadongzhe <327494410@qq.com>
2020-07-09 16:32:36 +0800

    init

---
+++ mbg/src/test/java/com/example/mbg/MbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mbg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
